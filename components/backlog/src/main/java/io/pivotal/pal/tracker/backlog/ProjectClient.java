710bff8c1c4c86b99addf72f4a9a76cf1f75c58e
shivacin <shivacin@yahoo.com>
2018-04-17 15:09:32 -0600

    Circuit Breaker Lab implemented

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,9 +1,18 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Logger;
 
 public class ProjectClient {
 
+    private final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
+    private Map<Long,ProjectInfo> concurrentMap = new ConcurrentHashMap<>();
     private final RestOperations restOperations;
     private final String endpoint;
 
@@ -12,7 +21,18 @@ public class ProjectClient {
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        //Retrieving the project based on the Id
+        ProjectInfo project = restOperations.getForObject(endpoint+"/projects"+projectId, ProjectInfo.class);
+        concurrentMap.put(projectId, project);
+        return project;
+
+    }
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+        logger.info("Getting project with id {} from cache"+ projectId);
+        //returning the Project Info based on the id passed
+        return concurrentMap.get(projectId);
     }
 }
